c07b4adb4e77aaa7d442b3b8fc61ad902fab3dfb
Yuki_Yamashita <yyper816@gmail.com>
2023-02-15 22:30:47 +0900

    3.7 基本型

---
+++ go/type/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main () {
+    fmt.Printf("uint32 max value = %d\n", math.MaxUint32)
+    fmt.Printf("float32 max value = %v\n", math.MaxFloat32)
+    fmt.Printf("float64 max value = %v\n", math.MaxFloat64)
+
+    i := 1
+    fmt.Printf("i is %T\n", i)
+
+    f64 := 1.0
+    fmt.Printf("f64 = 1.0 is %T\n", f64)
+
+    f32 := float32(1.0)
+    fmt.Printf("f32 = float32(1.0) is %T\n", f32)
+
+    zero := 0.0
+    fmt.Printf("zero = 0.0 is %v\n", zero)
+
+    pinf := 1.0 / zero
+    fmt.Printf("pinf is %v\n", pinf)
+
+    ninf := -1.0 / zero
+    fmt.Printf("ninf is %v\n", ninf)
+
+    nan := zero / zero
+    fmt.Printf("zero / zero = %v\n", nan)
+
+    c := complex(1.3, 4.2)
+    fmt.Printf("c's real is %v\n", real(c))
+    fmt.Printf("c's imag is %v\n", imag(c))
+
+    s := "GOの文字列"
+    fmt.Printf("%v\n", s)
+
+    raw := `
+        GO の
+        RAW文字列リテラルによる
+        複数行に渡る
+        文字列\n
+    `
+    fmt.Printf("%v", raw)
+}
